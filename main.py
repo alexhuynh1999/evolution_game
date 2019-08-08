be8a71e71a1f682301185c327443e23a0cccf3e0
Alex <alexhuynh1999@gmail.com>
2019-08-08 01:02:51 -0500

    added left/right up/down movement

--- main.py
+++ main.py
@@ -14,18 +14,44 @@ char = pygame.image.load('green ball.png')
 clock = pygame.time.Clock()
 switch = True
 
+
 def ball (x,y):
     display.blit(char, (x,y))
 
+
 x = width * 0.45
-y = height * 0.8
+y = 500
+x_vel = 0
+y_vel = 0
 
 while switch:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             switch = not switch
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_vel = -5
+            elif event.key == pygame.K_RIGHT:
+                x_vel = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_vel = 0
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y_vel = -5
+            elif event.key == pygame.K_DOWN:
+                y_vel = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_vel = 0
+
+    x += x_vel
+    y += y_vel
     display.fill(white)
-    ball(x,y)
+    ball(x, y)
+
     pygame.display.update()
     clock.tick(60)
 
